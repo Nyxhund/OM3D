11cc833589c72ea0266ec1902d1fcc65d001ff86
kerian.allaire <kerian.allaire@epita.fr>
2025-01-18 19:07:52 +0100

    feat: raymarching.

--- src/main.cpp
+++ src/main.cpp
@@ -379,7 +379,7 @@ std::unique_ptr<Scene> create_default_scene()
     {
         PointLight light;
         // light.set_position(glm::vec3(1.0f, 2.0f, 4.0f));
-        light.set_position(glm::vec3(2.0f, 10.0f, 2.0f));
+        light.set_position(glm::vec3(2.0f, 15.0f, 2.0f));
         light.set_color(glm::vec3(0.0f, 50.0f, 0.0f));
         light.set_radius(100.0f);
         // light.set_radius(4.0f);
